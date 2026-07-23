905da034b2f75779d5d1d479ac5c0bc0e94a4a4e
marwa youssef <marwayoussef678@gmail.com>
2026-07-23 23:56:27 +0300

    Add update product functionality in admin dashboard

--- src/Pages/Admin/AdminProducts/AdminProducts.jsx
+++ src/Pages/Admin/AdminProducts/AdminProducts.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Button,
   Container,
   Dropdown,
   Form,
   InputGroup,
-  ListGroup,
   Table,
 } from "react-bootstrap";
 import { CiSearch } from "react-icons/ci";
 import { api } from "../../../API/api";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { handleError } from "../../../utils/errorHandler";
+import { imageUrl } from "../../../utils/imageUrl";
+import { FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 export const AdminProducts = () => {
   //Loading State
@@ -19,18 +23,47 @@ export const AdminProducts = () => {
   //Navigation
   const navigate = useNavigate();
 
+  //products state
+  const [products, setProducts] = useState([]);
+
+  //======== fetch Admin Products =============//
+  async function fetchAdminProducts() {
+    try {
+      setLoading(true);
+
+      //Call API => api/v1/products
+      const response = await api.get("/products");
+      //set products
+      setProducts(response.data?.products);
+    } catch (error) {
+      handleError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchAdminProducts();
+  }, []);
+
+  //_________Delete Product _____________//
+  async function deleteProduct(id) {
+    try {
+      //call API => api/v1/delete/:id
+      await api.delete(`/products/delete/${id}`);
+      //Update UI
+      setProducts((prev) => prev.filter((product) => product._id !== id));
+    } catch (error) {
+      handleError(error);
+    }
+  }
+
+  if (loading) return <h1>Loading...</h1>;
   return (
     <Container fluid>
       {/* ===== Header ======== */}
       <div className="header">
         <h1>Products</h1>
-        <Button
-          onClick={() => {
-            navigate("/admin/products/add");
-          }}
-        >
-          + Add Product
-        </Button>
+        <Button>+ Add Product</Button>
       </div>
 
       {/* ======== Search  & Filter ======= */}
@@ -86,11 +119,52 @@ export const AdminProducts = () => {
               <th>Category</th>
               <th>Brand</th>
               <th>Price</th>
+              <th>discount price</th>
               <th>Stock</th>
               <th>Actions</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {products.length > 0 ? (
+              products.map((product) => (
+                <tr key={product._id}>
+                  <td>
+                    {" "}
+                    <img
+                      src={imageUrl(product.imageCover)}
+                      alt={product.name}
+                      width={50}
+                    />
+                  </td>
+                  <td>{product.name}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>{product.price} EGP</td>
+                  <td>{product.discountPrice} EGP</td>
+                  <td>{product.stock} </td>
+                  <Button
+                    onClick={() => {
+                      navigate(`/admin/products/update/${product._id}`);
+                    }}
+                  >
+                    <FaEdit />
+                  </Button>
+
+                  <Button
+                    onClick={() => {
+                      deleteProduct(product._id);
+                    }}
+                  >
+                    <FaTrash />
+                  </Button>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={7}>Product not found</td>
+              </tr>
+            )}
+          </tbody>
         </Table>
       </div>
     </Container>
--- src/Pages/Admin/UpdateProduct/UpdateProduct.jsx
+++ src/Pages/Admin/UpdateProduct/UpdateProduct.jsx
@@ -1,7 +1,172 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {  useNavigate, useParams } from 'react-router-dom';
+import { handleError } from '../../../utils/errorHandler';
+import { api } from '../../../API/api';
+import { Button, Form, Row } from 'react-bootstrap';
 
 export const UpdateProduct = () => {
+const navigate = useNavigate();
+const {id} = useParams(); 
+
+const [product,setProduct]= useState({
+  name:"",
+  description:"",
+  price:"",
+  discountPrice:"",
+  brand:"",
+  category:"",
+  stock:"",
+})
+//============ Fetch One Product By Id =============//
+async function fetchProductById(){
+  try {
+    //call api => api/v1/products/:id
+    const response = await api.get(`/products/${id}`)
+console.log(response.data.product)
+// to handle data required 
+  const fetchedProduct = response.data?.product;
+  setProduct({
+   name: fetchedProduct.name,
+  description: fetchedProduct.description,
+  price: fetchedProduct.price,
+  discountPrice: fetchedProduct.discountPrice,
+  brand: fetchedProduct.brand,
+  category: fetchedProduct.category,
+  stock: fetchedProduct.stock,
+  })
+    
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+useEffect(()=>{
+fetchProductById();
+},[id])
+
+
+
+
+
+
+//========== Handlers ==============//
+function handleChange(e) {
+  setProduct({
+    ...product,
+    [e.target.name]: e.target.value,
+  });
+}
+//Update Product
+async function updateProduct() {
+  try {
+    const response = await api.put(
+      `/products/update/${id}`,
+      product
+    );
+
+    console.log(response.data?.product);
+navigate("/admin/products")
+  } catch (error) {
+    handleError(error);
+  }
+}
   return (
-    <div>UpdateProduct</div>
+    <>
+  <div className="form-style">
+  <Form>
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label>Product Name</Form.Label>
+      <Form.Control
+        type="text"
+        name="name"
+        value={product.name}
+        onChange={handleChange}
+      />
+    </Form.Group>
+
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label>Description</Form.Label>
+      <Form.Control
+        as="textarea"
+        rows={4}
+        name="description"
+        value={product.description}
+        onChange={handleChange}
+      />
+    </Form.Group>
+
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label>Price</Form.Label>
+      <Form.Control
+        type="number"
+        min={0}
+        name="price"
+        value={product.price}
+        onChange={handleChange}
+      />
+    </Form.Group>
+
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label>Discount Price</Form.Label>
+      <Form.Control
+        type="number"
+        min={0}
+        name="discountPrice"
+        value={product.discountPrice}
+        onChange={handleChange}
+      />
+    </Form.Group>
+
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label>Stock</Form.Label>
+      <Form.Control
+        type="number"
+        min={0}
+        name="stock"
+        value={product.stock}
+        onChange={handleChange}
+      />
+    </Form.Group>
+
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label>Brand</Form.Label>
+      <Form.Control
+        type="text"
+        name="brand"
+        value={product.brand}
+        onChange={handleChange}
+      />
+    </Form.Group>
+
+    <Form.Group className="mb-3">
+      <Form.Label>Category</Form.Label>
+      <Form.Select
+        name="category"
+        value={product.category}
+        onChange={handleChange}
+      >
+        <option value="">Choose Category</option>
+        <option value="Electronics">Electronics</option>
+        <option value="Fashion">Fashion</option>
+        <option value="Home & Kitchen">Home & Kitchen</option>
+        <option value="Beauty">Beauty</option>
+        <option value="Sports">Sports</option>
+        <option value="Books">Books</option>
+      </Form.Select>
+    </Form.Group>
+
+    <Button
+      variant="primary"
+      onClick={updateProduct}
+    >
+      Update Product
+    </Button>
+  </Form>
+</div>
+    
+    
+    
+    
+    </>
   )
 }
